3106b0cac35b2b94cc45792d81c6dd41c0e25c8b
France <francevanmaanen@gmail.com>
2026-05-25 11:33:55 +0200

    Fix Terraform formatting

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,11 +2,11 @@ output "bucket_name" {
   description = "Name of the logs bucket"
   value       = aws_s3_bucket.logs.id
 }
- 
+
 output "bucket_arn" {
   description = "ARN of the logs bucket"
   value       = aws_s3_bucket.logs.arn
-} 
+}
 output "bucket_encryption" {
   description = "Encryption algorithm used"
   value       = "AES256"
